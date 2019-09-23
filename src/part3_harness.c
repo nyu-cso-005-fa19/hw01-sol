04561e7cda83a4a321d0ed74933147bedad6daf1
wies <wies@cs.nyu.edu>
2019-09-22 21:51:46 -0400

    one more time...

--- src/part3_harness.c
+++ src/part3_harness.c
@@ -64,7 +64,6 @@ void test_prime_number_sieves(int* a, int n) {
   prime_number_sieves_gold(a_gold, n);
 
   char* sa = array_to_string(a, n);
-  printf("%s\n", sa);
   
   for (int i = 2; i < n; i++) {
     panic_cond(a[i] == a_gold[i],
@@ -139,7 +138,7 @@ int main(int argc, char** argv)
   printf("part3: prime_number_sieves OK\n");
   
   printf("part3: testing find_smallest_divisor...\n");
-  for (int i = 0; i < 100; i++) {
+  for (int i = 2; i < 100; i++) {
     test_smallest_divisor(i);
   }
   printf("part3: find_smallest_divisor OK\n");
